9c78291e28bfe535eb8ba6865738d226cc21851c
Andrey Zavrichko <andrey@zavrichko.dev>
2022-12-05 19:32:32 +0200

    добавил тест на страницу Профиль

---
+++ src/test/java/com/dex/pages/ProfilePage.java
@@ -0,0 +1,144 @@
+package com.dex.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ProfilePage {
+    SelenideElement
+            authButton = $(byText("Вход")),
+            changePassButton = $(byText("Восстановить пароль")),
+            phoneInput = $("[name=phone]"),
+            smsCodeInput = $("[ type=text]"),
+            continueButton = $(byText("Продолжить")),
+            exitButton = $(byText("Выход")),
+            passwordInput = $("[name=password]"),
+            confirmPasswordInput = $("[name=confirmPassword]"),
+            profileButton = $("[fill=none]"),
+            profileLink = $(byText("Профиль")),
+            profileTitle = $(byText("Профиль пользователя")),
+            profileNameTitle = $(byText("Имя:")),
+            profileMiddleNameTitle = $(byText("Фамилия:")),
+            profileLastNameTitle = $(byText("Отчество:")),
+            rootElement = $("#root"),
+
+    profileBirthdayTitle = $(byText("Дата рождения:")),
+    profilePhoneTitle = $(byText("Телефон:")),
+
+    submitButton = $("[type=submit]"),
+            successPassTitle = $(byText("Вы успешно восстановили пароль")),
+
+
+    authRegButton = $(byText("Войти в аккаунт")),
+
+    confirmButton = $(byText("Подтвердить"));
+
+    public void openPage() {
+        open("/");
+    }
+
+    public void clickAuthButton() {
+        authButton.click();
+    }
+    public void clickProfileLink() {
+        profileLink.click();
+    }
+
+    public void clickAuthRegButton() {
+        authRegButton.click();
+    }
+
+    public void clickChangePassButton() {
+        changePassButton.click();
+    }
+
+    public void clickContinueButton() {
+        continueButton.click();
+    }
+
+    public void typePhone(String value) {
+        phoneInput.setValue(value);
+    }
+
+    public void clickConfirmButton() {
+        confirmButton.click();
+    }
+
+    public void typeSmsCode(String value) {
+        smsCodeInput.setValue(value);
+    }
+
+    public void typePassword(String value) {
+        passwordInput.setValue(value);
+    }
+
+    public void typeConfirmPassword(String value) {
+        confirmPasswordInput.setValue(value);
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+    public void checkSuccessPassTitle(String value) {
+        successPassTitle.shouldHave(text(value));
+    }
+    public void checkProfileTitle(String value) {
+        profileTitle.shouldHave(text(value));
+    }
+
+
+
+
+    public void clickProfileButton() {
+        profileButton.click();
+    }
+    public void clickExitButton() {
+        exitButton.click();
+    }
+
+    public void checkFirstName(String value) {
+        rootElement.shouldHave(text(value));
+    }
+
+    public void checkLastName(String value) {
+        rootElement.shouldHave(text(value));
+    }
+
+    public void checkMiddleName(String value) {
+        rootElement.shouldHave(text(value));
+    }
+
+    public void checkBirthday(String value) {
+        rootElement.shouldHave(text(value));
+    }
+
+    public void checkPhone(String value) {
+        rootElement.shouldHave(text(value));
+    }
+    public void checkProfileNameTitle(String value) {
+        profileNameTitle.shouldHave(text(value));
+    }
+
+    public void checkProfileMiddleNameTitle(String value) {
+        profileMiddleNameTitle.shouldHave(text(value));
+    }
+
+    public void checkProfileLastNameTitle(String value) {
+        profileLastNameTitle.shouldHave(text(value));
+    }
+
+    public void checkProfileBirthdayTitle(String value) {
+        profileBirthdayTitle.shouldHave(text(value));
+    }
+
+    public void checkProfilePhoneTitle(String value) {
+        profilePhoneTitle.shouldHave(text(value));
+    }
+
+
+
+
+}
---
+++ src/test/java/com/dex/tests/ProfileTest.java
@@ -0,0 +1,59 @@
+package com.dex.tests;
+
+import com.dex.TestBase;
+import com.dex.pages.AuthorizationPage;
+import com.dex.pages.ProfilePage;
+import com.dex.pages.RegistrationPage;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class ProfileTest extends TestBase {
+    ProfilePage profilePage = new ProfilePage();
+    AuthorizationPage authorizationPage = new AuthorizationPage();
+
+    @Test
+    @Feature("Профиль")
+    @Story("Профиль")
+    @DisplayName("Проверка экрана Профиль")
+    @Severity(SeverityLevel.BLOCKER)
+    void profilePositiveTest() {
+        step("Открываем сайт", () ->
+                authorizationPage.openPage());
+        step("Нажать кнопку Вход", () ->
+                authorizationPage.clickAuthButton());
+        step("Заполнить номер телефона", () ->
+                authorizationPage.typePhone("72355555524"));
+        step("Заполнить пароль", () ->
+                authorizationPage.typePassword("Passw0rd%"));
+        step("Нажать на кнопку Войти", () ->
+                authorizationPage.clickSubmit());
+        step("Проверить переход в админ-панель", () ->
+                authorizationPage.checkNewsLink());
+
+        step("Нажать кнопку Профиль", () ->
+                profilePage.clickProfileButton());
+        step("Нажать ссылку Профиль", () ->
+                profilePage.clickProfileLink());
+        step("Проверить содержимое страницы Профиль", () -> {
+            profilePage.checkProfileTitle("Профиль пользователя");
+            profilePage.checkFirstName("имя");
+            profilePage.checkLastName("фамилия");
+            profilePage.checkMiddleName("отчество");
+            profilePage.checkBirthday("08.12.2022");
+            profilePage.checkPhone("72355555524");
+            profilePage.checkProfileNameTitle("Имя");
+            profilePage.checkProfileLastNameTitle("Отчество");
+            profilePage.checkProfileMiddleNameTitle("Фамилия");
+            profilePage.checkProfileBirthdayTitle("Дата рождения");
+            profilePage.checkProfilePhoneTitle("Телефон");
+        });
+
+
+    }
+}
