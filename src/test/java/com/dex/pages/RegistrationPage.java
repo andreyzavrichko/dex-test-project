d15fc1dc24936a6eebddf63ab0c4a81746cc58ab
Andrey Zavrichko <andrey@zavrichko.dev>
2022-12-05 17:46:51 +0200

    добавил тест на регистрацию

---
+++ src/test/java/com/dex/pages/RegistrationPage.java
@@ -0,0 +1,114 @@
+package com.dex.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+    SelenideElement
+            authButton = $(byText("Вход")),
+            regButton = $(byText("Регистрация")),
+            phoneInput = $("[name=phone]"),
+
+            continueButton = $(byText("Продолжить")),
+            smsFormTitle = $(byText("подтвердите номер телефона")),
+            profileTitle = $(byText("укажите ваши данные")),
+            successTitle = $(byText("Вы успешно зарегестрировались")),
+            smsCodeInput = $("[ type=text]"),
+            firstNameInput = $("[name=firstName]"),
+            middleNameInput = $("[name=middleName]"),
+            lastNameInput = $("[name=lastName]"),
+            birthDayInput = $("[name=birthDay]"),
+            passwordInput = $("[name=password]"),
+            confirmPasswordInput = $("[name=confirmPassword]"),
+            confirmCheckbox = $("[for=checkBox]"),
+            submitButton = $("[type=submit]"),
+            newsLink = $(byText("новости")),
+            confirmButton = $(byText("Подтвердить"));
+
+
+    public void clickAuthButton() {
+        authButton.click();
+    }
+
+    public void clickRegButton() {
+        regButton.click();
+    }
+
+    public void clickContinueButton() {
+        continueButton.click();
+    }
+
+    public void openPage() {
+        open("/");
+    }
+
+    public void typePhone(String value) {
+        phoneInput.setValue(value);
+    }
+
+
+
+    public void typeSmsCode(String value) {
+        smsCodeInput.setValue(value);
+    }
+
+
+    public void checkNewsLink() {
+        newsLink.shouldBe(visible);
+    }
+
+    public void checkSmsFormTitle(String value) {
+        smsFormTitle.shouldHave(text(value));
+    }
+
+    public void checkProfileTitle(String value) {
+        profileTitle.shouldHave(text(value));
+    }
+
+
+    public void clickSubmit() {
+        submitButton.click();
+    }
+
+    public void typeFirstName(String value) {
+        firstNameInput.setValue(value);
+    }
+
+    public void typeLastName(String value) {
+        lastNameInput.setValue(value);
+    }
+
+    public void typeMiddleName(String value) {
+        middleNameInput.setValue(value);
+    }
+    public void typeBirthDay(String value) {
+        birthDayInput.setValue(value);
+    }
+
+    public void typePassword(String value) {
+        passwordInput.setValue(value);
+    }
+
+    public void typeConfirmPassword(String value) {
+        confirmPasswordInput.setValue(value);
+    }
+
+    public void typeConfirmCheckbox() {
+        confirmCheckbox.click();
+    }
+
+    public void typeConfirmButton() {
+        confirmButton.click();
+    }
+
+    public void checkSuccessTitle(String value) {
+        successTitle.shouldHave(text(value));
+    }
+
+
+}
---
+++ src/test/java/com/dex/tests/RegistrationTest.java
@@ -0,0 +1,121 @@
+package com.dex.tests;
+
+import com.dex.TestBase;
+import com.dex.pages.AuthorizationPage;
+import com.dex.pages.RegistrationPage;
+import com.github.javafaker.Faker;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Регистрация")
+public class RegistrationTest extends TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+    Faker faker = new Faker();
+    String phone = "7" + faker.number().digits(10);
+    String smsCode = "0000";
+    String firstName = faker.name().firstName();
+    String middleName = faker.name().lastName();
+    String lastName = faker.animal().name();
+    String birthDay = "12-02-1990";
+    String password = "Passw0rd%";
+    String confirmPassword = "Passw0rd%";
+
+
+
+    @Test
+    @Feature("Регистрация")
+    @Story("Регистрация")
+    @DisplayName("Регистрация на новый номер - проверка перехода на экран смс кода")
+    @Severity(SeverityLevel.BLOCKER)
+    void registrationFirstTest() {
+        step("Открываем сайт", () ->
+                registrationPage.openPage());
+        step("Нажать кнопку Вход", () ->
+                registrationPage.clickAuthButton());
+        step("Нажать ссылку Регистрация", () ->
+                registrationPage.clickRegButton());
+        step("Заполнить номер телефона", () ->
+                registrationPage.typePhone(phone));
+        step("Нажать на кнопку Продолжить", () ->
+                registrationPage.clickContinueButton());
+        step("Проверить переход к экрану смс кода", () ->
+                registrationPage.checkSmsFormTitle("подтвердите номер телефона"));
+    }
+
+    @Test
+    @Feature("Регистрация")
+    @Story("Регистрация")
+    @DisplayName("Регистрация на новый номер - проверка перехода на экран создания профиля")
+    @Severity(SeverityLevel.BLOCKER)
+    void registrationSmsCodeTest() {
+        step("Открываем сайт", () ->
+                registrationPage.openPage());
+        step("Нажать кнопку Вход", () ->
+                registrationPage.clickAuthButton());
+        step("Нажать ссылку Регистрация", () ->
+                registrationPage.clickRegButton());
+        step("Заполнить номер телефона", () ->
+                registrationPage.typePhone(phone));
+        step("Нажать на кнопку Продолжить", () ->
+                registrationPage.clickContinueButton());
+        step("Ввести код из смс", () ->
+                registrationPage.typeSmsCode(smsCode));
+        step("Нажать на кнопку Продолжить", () ->
+                registrationPage.clickContinueButton());
+        step("Проверить переход к экрану создания профиля", () ->
+                registrationPage.checkProfileTitle("укажите ваши данные"));
+    }
+
+    @Test
+    @Feature("Регистрация")
+    @Story("Регистрация")
+    @DisplayName("Регистрация на новый номер - проверка успешного создания")
+    @Severity(SeverityLevel.BLOCKER)
+    void registrationProfileTest() {
+        step("Открываем сайт", () ->
+                registrationPage.openPage());
+        step("Нажать кнопку Вход", () ->
+                registrationPage.clickAuthButton());
+        step("Нажать ссылку Регистрация", () ->
+                registrationPage.clickRegButton());
+        step("Заполнить номер телефона", () ->
+                registrationPage.typePhone(phone));
+        step("Нажать на кнопку Продолжить", () ->
+                registrationPage.clickContinueButton());
+        step("Ввести код из смс", () ->
+                registrationPage.typeSmsCode(smsCode));
+        step("Нажать на кнопку Продолжить", () ->
+                registrationPage.clickContinueButton());
+        step("Заполнить поле имя", () ->
+                registrationPage.typeFirstName(firstName));
+        step("Заполнить поле фамилия", () ->
+                registrationPage.typeMiddleName(middleName));
+        step("Заполнить поле отчество", () ->
+                registrationPage.typeLastName(lastName));
+        step("Заполнить поле Дата рождения", () ->
+                registrationPage.typeBirthDay(birthDay));
+        step("Заполнить поле Пароль", () ->
+                registrationPage.typePassword(password));
+        step("Заполнить поле Подтвердить пароль", () ->
+                registrationPage.typeConfirmPassword(confirmPassword));
+        step("Установить чек-бокс", () ->
+                registrationPage.typeConfirmCheckbox());
+        step("Нажимаем кнопку Зарегистрироваться", () ->
+                registrationPage.clickSubmit());
+        //Костыль, т.к. кнопка со второго раза срабатывает
+        step("Нажимаем кнопку Зарегистрироваться", () ->
+                registrationPage.clickSubmit());
+        step("Проверить переход к экрану создания профиля", () ->
+                registrationPage.checkSuccessTitle("Вы успешно зарегестрировались"));
+    }
+
+
+
+
+}
