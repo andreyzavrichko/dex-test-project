8f692a8d92f2fbc19e18a402861c078a5f936625
Andrey Zavrichko <andrey@zavrichko.dev>
2022-12-06 17:27:30 +0200

    обновил зависимости, прогнал через линтер код

--- src/test/java/com/dex/TestBase.java
+++ src/test/java/com/dex/TestBase.java
@@ -5,9 +5,6 @@ import com.dex.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-
-
-import static com.codeborne.selenide.FileDownloadMode.PROXY;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
@@ -16,13 +13,7 @@ public class TestBase {
         Configuration.browserSize = "1600x780";
         Configuration.baseUrl = "https://demo1.k3s.dex-it.ru";
 
-        // Конфигурация для скачивания файлов
-        Configuration.downloadsFolder = "src/test/resources/files";
-
-       // Configuration.proxyEnabled = true;
-       // Configuration.fileDownload = PROXY;
-
-        // Конфигурация для удаленного запуска
+         // Конфигурация для удаленного запуска
 //        Configuration.remote = "http://149.154.70.38:4444/wd/hub";
 //        Configuration.remote = System.getProperty("url");
 //        Configuration.browser = System.getProperty("browser");
