c5e002dcb421709942e9207719e4dd43c4e13ea9
Andrey Zavrichko <andrey@zavrichko.dev>
2022-12-07 21:04:43 +0200

    добавил тест Проверка главной страницы

---
+++ src/test/java/com/dex/pages/MainPage.java
@@ -0,0 +1,36 @@
+package com.dex.pages;
+
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+    SelenideElement
+            header = $("header"),
+            main = $("main"),
+            headerLogo = $("[alt=image]"),
+            mainLogo = $("[alt=MainPageLogo]");
+
+
+    public void checkHeader() {
+        header.shouldBe(visible);
+    }
+
+    public void checkMain() {
+        main.shouldBe(visible);
+    }
+
+    public void checkHeaderLogo() {
+        headerLogo.shouldBe(visible);
+    }
+
+    public void checkMainLogo() {
+        mainLogo.shouldBe(visible);
+    }
+
+
+}
+
+
+
---
+++ src/test/java/com/dex/tests/MainTest.java
@@ -0,0 +1,39 @@
+package com.dex.tests;
+
+import com.dex.TestBase;
+import com.dex.pages.AuthorizationPage;
+import com.dex.pages.MainPage;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+class MainTest extends TestBase {
+    AuthorizationPage authorizationPage = new AuthorizationPage();
+    MainPage mainPage = new MainPage();
+
+    @Test
+    @Feature("Главная")
+    @Story("Главная")
+    @DisplayName("Проверка главной страницы")
+    @Severity(SeverityLevel.BLOCKER)
+    void mainTest() {
+        step("Открываем сайт", () ->
+                authorizationPage.openPage());
+        step("Проверяем наличие всех элементов на странице", () -> {
+            mainPage.checkHeader();
+            mainPage.checkHeaderLogo();
+            mainPage.checkMain();
+            mainPage.checkMainLogo();
+        });
+
+    }
+
+
+
+
+}
