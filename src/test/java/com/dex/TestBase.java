c8a4bed9faee6d2a7477a4060dbabd1299e29678
Andrey Zavrichko <andrey@zavrichko.dev>
2022-12-10 11:20:19 +0200

    настройки для CI

--- src/test/java/com/dex/TestBase.java
+++ src/test/java/com/dex/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.dex.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -16,14 +17,14 @@ public class TestBase {
 
         // Конфигурация для удаленного запуска
 //        Configuration.remote = "http://149.154.70.38:4444/wd/hub";
-//        Configuration.remote = System.getProperty("url");
-//        Configuration.browser = System.getProperty("browser");
-//        Configuration.browserVersion = System.getProperty("version");
+        Configuration.remote = System.getProperty("url");
+        Configuration.browser = System.getProperty("browser");
+       Configuration.browserVersion = System.getProperty("version");
 
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableVideo", true);
-//        Configuration.browserCapabilities = capabilities;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
